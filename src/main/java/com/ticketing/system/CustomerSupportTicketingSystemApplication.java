36857b046379991860a26f8a999e60293cdf9cc0
Swochhandita Ghimire <swochhanditaghimire@gmail.com>
2026-03-19 14:19:58 +0545

    First commit of the customer support ticketing system

---
+++ src/main/java/com/ticketing/system/CustomerSupportTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ticketing.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSupportTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSupportTicketingSystemApplication.class, args);
+	}
+
+}
